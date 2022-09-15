5e7155394259fdbcacc5220b74c25369b030d4ed
Vladimir_Chernyaev <>
2022-09-15 22:06:51 +0400

    added first programm

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// программа котора принимает на вход два числа (А и B) 
+// и возводит число A в натуральную степень B
+Console.WriteLine("Введите число A ");
+int NumberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число В ");
+int NumberB = Convert.ToInt32(Console.ReadLine());
+
+int exponent (int num1, int num2)
+{
+    int result = num1;
+    for (int i=1; num2>i; i++)
+    {
+       result  = result*num1;
+    }
+    return result;
+}    
+ Console.WriteLine($"{NumberA} в степени {NumberB} равно {exponent(NumberA, NumberB)}");  
